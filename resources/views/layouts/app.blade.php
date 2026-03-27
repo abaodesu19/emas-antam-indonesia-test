b64e287637866f7842b1e40f42aee1fb26b7460c
Muhammad Akbar <abaodesu@gmail.com>
2026-03-27 16:42:36 +0700

    Tambahan Seeder Product

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,8 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
  
-        <title>{{ $title ?? config('app.name') }}</title>
+        <title>Muhammad Akbar | PT Emas Antam Indonesia Coding Test</title>
+        <link rel="icon" href="https://emasantam.id/wp-content/uploads/2022/01/cropped-Master-Logo-gunung-kotak-kecil-32x32.png" type="image/png">
  
         @vite(['resources/css/app.css', 'resources/js/app.js'])
  
