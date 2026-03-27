a7c37de49b04f9a9d593957ce075a7b5adada943
Muhammad Akbar <abaodesu@gmail.com>
2026-03-27 17:06:40 +0700

    Tambahan Seeder Product

--- database/migrations/2026_03_27_084402_create_products_table.php
+++ database/migrations/2026_03_27_084402_create_products_table.php
@@ -10,7 +10,7 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->integer('id')->autoIncrement(); 
-            $table->string('name');
+            $table->string('name')->unique();
             $table->decimal('price', 15, 2);
             $table->timestamps();
         });
